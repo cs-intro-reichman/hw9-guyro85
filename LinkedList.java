e3362d7bd7d60fbc6ba0eb9e8c27cab74cab723b
guyro85 <guynevo8@gmail.com>
2025-01-25 21:41:50 +0200

    testing 2

--- LinkedList.java
+++ LinkedList.java
@@ -191,9 +191,9 @@ public class LinkedList {
 	 *        the node that will be removed from this list
 	 */
 	public void remove(Node node) {
-		if (node == null || size == 0){
-			throw new IllegalArgumentException(
-					"index must be between 0 and size");
+		if (node == null){
+			throw new NullPointerException(
+					" NullPointerException!");
 		}
 		if (node.equals(first)){
 			first = first.next;
